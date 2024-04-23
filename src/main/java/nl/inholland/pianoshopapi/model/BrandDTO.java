f546d4c7c4fd54db86928f3eb554d5f60f776fad
mrwim <willem.wiltenburg@inholland.nl>
2024-04-23 23:00:15 +0200

    Main branch

--- src/main/java/nl/inholland/pianoshopapi/model/BrandDTO.java
+++
@@ -1,4 +0,0 @@
-package nl.inholland.pianoshopapi.model;
-
-public record BrandDTO(String name, String description) {
-}
--- src/main/java/nl/inholland/pianoshopapi/model/PianoDTO.java
+++
@@ -1,4 +0,0 @@
-package nl.inholland.pianoshopapi.model;
-
-public record PianoDTO(String brand, String model, int year) {
-}
